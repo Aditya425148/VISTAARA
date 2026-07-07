770b5cde1f5e4f2ea46f01f66f4485a35bceaf9e
Aditya425148 <aadi100right@gmail.com>
2026-07-08 01:57:07 +0530

    Delete client directory

--- client/src/components/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-
-export default function ProtectedRoute({ children, admin = false }) {
-  const { user } = useSelector((state) => state.auth);
-  if (!user) return <Navigate to="/login" replace />;
-  if (admin && user.role !== 'admin') return <Navigate to="/" replace />;
-  return children;
-}
